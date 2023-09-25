85a795bfeacb0a44c6ad9aa6dbe7fd2a5e37a207
Vithor Hernandez <142887134+VithorDev011@users.noreply.github.com>
2023-09-24 21:06:29 -0300

    adicionei o Javascript

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,6 @@
 let numeroatualspan = document.getElementById("currentNumber");
 let currentNumber = 0;
 
-document.addEventListener('touchstart', function(event) {
-    if (event.touches.length > 1) {
-        event.preventDefault();
-    }
-}, { passive: false });
-
-document.getElementById("subtrair").addEventListener("click", function() {
-    // Bloqueia o zoom do botão subtrair
-    document.querySelector("meta[name=viewport]").setAttribute("content", "width=device-width, initial-scale=1.0, user-scalable=no");
-});
-
-document.getElementById("limpar").addEventListener("click", function() {
-    // Bloqueia o zoom do botão limpar
-    document.querySelector("meta[name=viewport]").setAttribute("content", "width=device-width, initial-scale=1.0, user-scalable=no");
-});
-
-document.getElementById("adicionar").addEventListener("click", function() {
-    // Bloqueia o zoom do botão adicionar
-    document.querySelector("meta[name=viewport]").setAttribute("content", "width=device-width, initial-scale=1.0, user-scalable=no");
-});
-
 function resetColor() {
     if (currentNumber == 0) {
         document.getElementById("currentNumber").style.color = "black";
